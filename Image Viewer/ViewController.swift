18e8faa5ea97248baad72a10b7ea767cb2fa6727
Jonghyun Kim <jhkim@dit.ac.kr>
2019-03-26 11:54:43 +0900

    Initial Commit

---
+++ Image Viewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Image Viewer
+//
+//  Created by amadeus on 26/03/2019.
+//  Copyright © 2019 DIT Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
